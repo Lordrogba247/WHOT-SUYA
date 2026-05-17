94dc247f38b71d6303b6230e65ffd0e159ddb835
Lordrogba247 <adedapomichael55@gmail.com>
2026-05-17 22:10:52 +0100

    Updated changes 5

--- src/Landing/Landing.jsx
+++ src/Landing/Landing.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Landing.css";
 
 import IMG1 from "../assets/pepper.png";
@@ -8,6 +9,8 @@ import IMG4 from "../assets/pepper-two.png";
 import textImg from "../assets/text.png";
 
 export default function Landing() {
+    const navigate = useNavigate();
+
     return (
         <div className="landing">
 
@@ -26,7 +29,11 @@ export default function Landing() {
                         Satisfy your cravings in minutes. From local delicacies
                         to international flavors. We've got you covered.
                     </p>
-                    <button className="btn-order show-2">Order Now ↗</button>
+                    <button className="btn-order show-2" onClick={() => navigate("/menu")}>Order Now <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="2em" viewBox="0 0 24 24">
+                        <path d="M0 0h24v24H0z" fill="none" />
+                        <path fill="currentColor" d="M5 17.59L15.59 7H9V5h10v10h-2V8.41L6.41 19z" />
+                    </svg>
+                    </button>
                 </div>
 
                 <div className="hero-right">
@@ -39,7 +46,10 @@ export default function Landing() {
                     Satisfy your cravings in minutes. From local delicacies
                     to international flavors. We've got you covered.
                 </p>
-                <button className="btn-order show-3">Order Now ↗</button>
+                <button className="btn-order show-3" onClick={() => navigate("/menu")}>Order Now <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20">
+                    <path d="M0 0h24v24H0z" fill="none" />
+                    <path fill="currentColor" d="M5 17.59L15.59 7H9V5h10v10h-2V8.41L6.41 19z" />
+                </svg></button>
 
             </section>
 
